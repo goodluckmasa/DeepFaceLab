c46803257dba72e72df8d3a2bf806c62cfd1d0e0
jh <jmhummel@gmail.com>
2021-03-12 11:44:59 -0800

    test: more debuging

--- core/leras/ops/__init__.py
+++ core/leras/ops/__init__.py
@@ -319,13 +319,13 @@ def ms_ssim(img1, img2, resolution, kernel_size=11, k1=0.01, k2=0.03, max_value=
     # if sum(power_factors) < 1.0:
     #     power_factors = [x/sum(power_factors) for x in power_factors]
 
-    img_dtype = img1.dtype
-    if img_dtype != img2.dtype:
-        raise ValueError("img1.dtype != img2.dtype")
+    # img_dtype = img1.dtype
+    # if img_dtype != img2.dtype:
+    #     raise ValueError("img1.dtype != img2.dtype")
 
-    if img_dtype != tf.float32:
-        img1 = tf.cast(img1, tf.float32)
-        img2 = tf.cast(img2, tf.float32)
+    # if img_dtype != tf.float32:
+    #     img1 = tf.cast(img1, tf.float32)
+    #     img2 = tf.cast(img2, tf.float32)
 
     # Transpose images from NCHW to NHWC
     img1_t = tf.transpose(img1, [0, 2, 3, 1])
@@ -334,8 +334,8 @@ def ms_ssim(img1, img2, resolution, kernel_size=11, k1=0.01, k2=0.03, max_value=
                                            filter_size=kernel_size, k1=k1, k2=k2)
     ms_ssim_loss = (1.0 - ms_ssim_val) / 2.0
 
-    if img_dtype != tf.float32:
-        ms_ssim_loss = tf.cast(ms_ssim_loss, img_dtype)
+    # if img_dtype != tf.float32:
+    #     ms_ssim_loss = tf.cast(ms_ssim_loss, img_dtype)
     return ms_ssim_loss
 
 nn.ms_ssim = ms_ssim
